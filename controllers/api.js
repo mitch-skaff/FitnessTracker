3df12289c08982b5169ef48a975823152b8f9079
mskaff <mskaff904@gmail.com>
2021-08-16 19:57:19 -0400

    fixed bug in server.js and started building out api/view routes

--- controllers/api.js
+++ controllers/api.js
@@ -1,3 +1,46 @@
 const router = require("express").Router();
-const Workout = require("../models/workout");
+const Workout = require("../models/workout.js");
 
+// pull info from db for the workouts page
+router.get("/workouts", (req, res) => {
+    Workout.find({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+router.get("/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+// route to submit new completed workouts
+router.post("/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+// route to update mongodb exercise by id value 
+router.put("/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true })
+        .then(data => { res.json(data); })
+        .catch(err => { res.json(err); });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = rquire("mongoose");
+const mongoose = require("mongoose");
 const controllers = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
