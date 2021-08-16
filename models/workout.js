241e0e2f97f5db771a99741a686185f95ca1716d
mskaff <mskaff904@gmail.com>
2021-08-16 18:44:04 -0400

    created necessary files in MVC format and built out models

---
+++ models/workout.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+        }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
